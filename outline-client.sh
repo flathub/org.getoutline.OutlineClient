5b2db977d7c0abaa98c7458d6c9fba19853ed413
proletarius101 <54175165+proletarius101@users.noreply.github.com>
2026-02-23 23:43:14 -0800

    Rename outline-client executable to Outline

--- outline-client.sh
+++ outline-client.sh
@@ -5,4 +5,4 @@ set -oue pipefail
 export FLATPAK_ID="${FLATPAK_ID:-org.getoutline.OutlineClient}"
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
-exec zypak-wrapper /app/outline-client/outline-client $@
+exec zypak-wrapper /app/outline-client/Outline $@
